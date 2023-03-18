35a1da0030c0699d3990bdd0d1d5622ed82eab31
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-03-18 15:33:24 +0100

    18-03

---
+++ js/reweu.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openReweuBtn: document.querySelector("[data-reweu-open]"),
+      closeReweuBtn: document.querySelector("[data-reweu-close]"),
+      reweu: document.querySelector("[data-reweu]"),
+    };
+  
+    refs.openReweuBtn.addEventListener("click", toggleReweu);
+    refs.closeMReweuBtn.addEventListener("click", toggleReweu);
+  
+    function toggleReweu() {
+      refs.reweu.classList.toggle("is-hidden");
+    }
+  })();
