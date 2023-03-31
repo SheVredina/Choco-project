a54b2817a63c34f6b82c65307b30a33afaabf9f0
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-03-31 16:29:54 +0200

    create

---
+++ js/test.js
@@ -0,0 +1,12 @@
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//     // Change code below this line
+//     const totalPrice = orderedQuantity * pricePerItem;
+  
+//     // Change code above this line
+//     return totalPrice;
+//   };
+
+// результат будет виден в консоли
+for (var i = 0; i < 6; i++) {
+    console.log("значение", i);
+  }
