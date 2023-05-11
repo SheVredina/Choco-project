a14f19cbb849520f7d2c6bdfd7b544bb7088114e
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-05-11 15:03:35 +0200

    1

--- js/test.js
+++ js/test.js
@@ -1,12 +1,35 @@
-// function calculateTotalPrice (orderedQuantity, pricePerItem) {
-//     // Change code below this line
-//     const totalPrice = orderedQuantity * pricePerItem;
-  
-//     // Change code above this line
-//     return totalPrice;
-//   };
+class User {
+  // Объявление и инициализация статического свойства
+  static Roles = {
+    ADMIN: "admin",
+    EDITOR: "editor",
+  };
 
-// результат будет виден в консоли
-for (var i = 0; i < 6; i++) {
-    console.log("значение", i);
+  #email;
+  #role;
+
+  constructor({ email, role }) {
+    this.#email = email;
+    this.#role = role;
   }
+
+  get role() {
+    return this.#role;
+  }
+
+  set role(newRole) {
+    this.#role = newRole;
+  }
+}
+
+const mango = new User({
+  email: "mango@mail.com",
+  role: User.Roles.ADMIN,
+});
+
+console.log(mango.Roles); // undefined
+console.log(User.Roles); // { ADMIN: "admin", EDITOR: "editor" }
+
+console.log(mango.role); // "admin"
+mango.role = User.Roles.EDITOR;
+console.log(mango.role); // "editor"
